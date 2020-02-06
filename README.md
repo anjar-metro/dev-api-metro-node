35889f6785254a39eebe886f3b9f9fe6b50b1c1e
Anjar Widiyatmoko <anjar@metroney.com>
2020-02-06 20:49:37 +0700

    Add  MongoDB Connection

---
+++ README.md
@@ -0,0 +1,3 @@
+# README.md
+MongoDB Docker:
+mongoadmin/mongoadmin
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
+var mongoClient = require('mongodb').MongoClient
 const app = express()
 const port = 5000
 const route = require('./src/route')
-
 
 route.init(app)
 
@@ -10,5 +10,22 @@ app.use(express.static('images'))
 
 //Serves all the request which includes /images in the url from Images folder
 app.use('/images', express.static(__dirname + '/images'))
+
+// make client connect to mongo service
+let url = "mongodb://mongoadmin:mongoadmin@localhost:27018"; 
+mongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Connected to Database!");
+    // print database name
+    // console.log("db object points to the database : "+ db.databaseName);
+
+    var dbo = db.db("metrotest01");
+    dbo.collection("area").find({}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+    });
+     
+}); 
 
 app.listen(port, () => console.log(`App Listening on port ${port}`))
